849a0b9f89f0e73c6e6370acdc3a52b9d9f757cb
yarra003 <yarra.hani2003@gmail.com>
2024-09-04 09:51:50 +0300

    End of section 10

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -5,29 +5,32 @@ using API.Entities;
 using Microsoft.IdentityModel.Tokens;
 
 namespace API;
-class TokenService(IConfiguration config) : ITokenService
+
+public class TokenService(IConfiguration config) : ITokenService
 {
     public string CreateToken(AppUser user)
     {
-        var tokenKey= config["TokenKey"] ?? throw new Exception("Cannot access tokenKey from app settings") ;
-        if (tokenKey.Length<64) throw new Exception("Your tokenKey needs to be longer");
-        var key= new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+        var tokenKey = config["TokenKey"] ?? throw new Exception("Cannot access tokenKey from appsettings");
+        if (tokenKey.Length < 64) throw new Exception("Your tokenKey needs to be longer");
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
 
-        var claims= new List<Claim>{
+        var claims = new List<Claim>
+        {
             new(ClaimTypes.NameIdentifier, user.UserName)
         };
 
-        var creds=new SigningCredentials(key,SecurityAlgorithms.HmacSha512Signature);
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
-        var tokenDescriptor= new SecurityTokenDescriptor{
-        Subject= new ClaimsIdentity(claims),
-        Expires= DateTime.UtcNow.AddDays(7),
-        SigningCredentials=creds
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = creds
         };
-        var tokenHandler= new JwtSecurityTokenHandler();
-        var token= tokenHandler.CreateToken(tokenDescriptor);
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
         return tokenHandler.WriteToken(token);
-        
     }
-    
 }
